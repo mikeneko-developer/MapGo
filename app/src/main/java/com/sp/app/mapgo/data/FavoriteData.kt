ed85eca1cfe38414ea774964373606bc0fda23e4
mikeneko-developer <mikemobiler.developer@gmail.com>
2024-05-31 21:55:05 +0900

    不要コード削除

--- app/src/main/java/com/sp/app/mapgo/data/FavoriteData.kt
+++
@@ -1,9 +0,0 @@
-package com.sp.app.mapgo.data
-
-data class FavoriteData(var id: Int,
-                        var name: String,
-                        var address: String,
-                        var lat: Double,
-                        var lon: Double){
-    constructor():this(0,"","",0.0,0.0)
-}
